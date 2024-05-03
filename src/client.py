b43cc9a5c4026fe29a0ad1355edd7a0704000aae
Delos-343 <mfachry.handoko@gmail.com>
2024-05-03 16:10:59 +0800

    first commit

---
+++ src/client.py
@@ -0,0 +1,39 @@
+import socket
+import select
+import sys
+from threading import Thread
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = '127.0.0.1'
+
+port = 8081
+
+server.connect((ip_address, port))
+
+def send_msg(sock):
+  while True:
+    data = sys.stdin.readline()
+    sock.send(data.encode())
+    sys.stdout.write('\n<Urself>')
+    sys.stdout.write(' ' + data)
+    sys.stdout.flush()
+
+def recv_msg(sock):
+  while True:
+    data = sock.recv(2048)
+    sys.stdout.write(' ' + data.decode())
+
+Thread(target=send_msg, args=(server,)).start()
+Thread(target=recv_msg, args=(server,)).start()
+
+while True:
+  sockets_list = [server]
+  read_socket, write_socket, error_socket = select.select(sockets_list, [], [])
+  for socks in read_socket:
+    if socks == server:
+      recv_msg(socks)
+    else:
+      send_msg(socks)
+
+server.close()
