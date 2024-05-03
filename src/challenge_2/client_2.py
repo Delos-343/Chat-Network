2580f88138e3e15dd5ce5393f3c9cda18cea9404
Delos-343 <mfachry.handoko@gmail.com>
2024-05-03 19:33:48 +0800

    Found a solution for Challenge 2

--- src/challenge_2/client_2.py
+++ src/challenge_2/client_2.py
@@ -4,25 +4,21 @@ import sys
 from threading import Thread
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 ip_address = '127.0.0.1'
-
 port = 8081
-
 server.connect((ip_address, port))
 
 def send_msg(sock):
-  while True:
-    data = sys.stdin.readline()
-    sock.send(data.encode())
-    sys.stdout.write('\n<Urself>')
-    sys.stdout.write(' ' + data)
-    sys.stdout.flush()
+    while True:
+        data = sys.stdin.readline().strip()
+        if data.lower() == "list":
+            sys.stdout.write("Requesting list of participants...\n")
+        sock.send(data.encode())
 
 def recv_msg(sock):
-  while True:
-    data = sock.recv(2048)
-    sys.stdout.write(' ' + data.decode())
+    while True:
+        data = sock.recv(2048)
+        sys.stdout.write(data.decode() + '\n')
 
 Thread(target=send_msg, args=(server,)).start()
 Thread(target=recv_msg, args=(server,)).start()
--- src/challenge_2/server_2.py
+++ src/challenge_2/server_2.py
@@ -4,50 +4,75 @@ import sys
 import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 ip_address = '127.0.0.1'
-
 port = 8081
 
 server.bind((ip_address, port))
-
 server.listen(100)
 
-list_of_clients = []
+list_of_clients = {}
+client_id_counter = 1
 
 def clientthread(conn, addr):
-  while True:
-    try:
-      message = conn.recv(2048).decode()
-      if message:
-        if message:
-          print('<' + addr[0] + '>' + message)
-          message_to_send = '<' + addr[0] + '>' + message
-          broadcast(message_to_send, conn)
-      else:
-        remove(conn)
-    except:
-      continue
+    global client_id_counter
+    conn.send(f"Welcome to the chat! Your ID is {client_id_counter}\n".encode())
+    client_id = client_id_counter
+    list_of_clients[client_id] = conn
+    client_id_counter += 1
+
+    while True:
+        try:
+            message = conn.recv(2048).decode()
+            if message:
+                if message.lower() == "list":
+                    send_client_list(conn)
+                elif message.startswith("private"):
+                    handle_private_message(message, client_id)
+                else:
+                    broadcast(f'<Client {client_id}> {message}', conn)
+            else:
+                remove(conn)
+        except Exception as e:
+            print(e)
+            continue
+
+def send_client_list(conn):
+    conn.send("List of participants:\n".encode())
+    for client_id in list_of_clients:
+        if client_id != conn:
+            conn.send(f"Client {client_id}\n".encode())
+
+def handle_private_message(message, sender_id):
+    parts = message.split(maxsplit=2)
+    if len(parts) == 3:
+        dest_id = int(parts[1])
+        dest_conn = list_of_clients.get(dest_id)
+        if dest_conn:
+            dest_conn.send(f'<Private from {sender_id}> {parts[2]}'.encode())
+        else:
+            list_of_clients[sender_id].send(f"Client {dest_id} is not connected.\n".encode())
+    else:
+        list_of_clients[sender_id].send("Invalid private message syntax.\n".encode())
 
 def broadcast(message, connection):
-  for clients in list_of_clients:
-    if clients != connection:
-      try:
-        clients.send(message.encode())
-      except:
-        clients.close()
-        remove(clients)
+    for client_id, client_conn in list_of_clients.items():
+        if client_conn != connection:
+            try:
+                client_conn.send(message.encode())
+            except:
+                client_conn.close()
+                remove(client_conn)
 
 def remove(connection):
-  if connection in list_of_clients:
-    list_of_clients.remove(connection)
+    for client_id, client_conn in list_of_clients.items():
+        if client_conn == connection:
+            del list_of_clients[client_id]
+            break
 
 while True:
-  conn, addr = server.accept()
-  list_of_clients.append(conn)
-  print('\n' + addr[0] + ' ~ connected')
-  threading.Thread(target=clientthread, args=(conn, addr)).start()
+    conn, addr = server.accept()
+    threading.Thread(target=clientthread, args=(conn, addr)).start()
 
 conn.close()
